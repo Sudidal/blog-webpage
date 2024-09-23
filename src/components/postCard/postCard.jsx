cc29eff569c8f9c7c592c8665bd5c65b6f504cb4
oxide <mohammedhaider15mo@gmail.com>
2024-09-23 08:26:25 +0300

    Feat: make post cards link to post content

--- src/components/postCard/postCard.jsx
+++ src/components/postCard/postCard.jsx
@@ -1,15 +1,16 @@
 import classes from "./postCard.module.css"
+import { Link } from "react-router-dom";
 
 import PropTypes from "prop-types";
 
 function PostCard({ post }) {
   return (
-    <div className={classes.postCard}>
+    <Link to={"/posts/" + post.id} className={classes.postCard}>
       <p>{post.title}</p>
       <p>{post.author.username}</p>
       <p>{post.publishDate}</p>
       <p>{post.comments.length}</p>
-    </div>
+    </Link>
   );
 }
 
