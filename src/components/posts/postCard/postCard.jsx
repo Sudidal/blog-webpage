c1a59a101285869da29c8a9416207c5fc45833e0
oxide <mohammedhaider15mo@gmail.com>
2024-09-29 15:32:21 +0300

    Refac: Use props on post to detirmine permissions

--- src/components/posts/postCard/postCard.jsx
+++ src/components/posts/postCard/postCard.jsx
@@ -1,51 +1,51 @@
 import classes from "./postCard.module.css";
 import PropTypes from "prop-types";
-import blogApi from "../../../blogAPI.js";
 import { useNavigate } from "react-router-dom";
-import { useContext } from "react";
-import { userContext } from "../../../contexts/userContext.jsx";
 import IconWithCount from "../../iconWithCount/iconWithCount.jsx";
 import IconButton from "../../iconButton/iconButton.jsx";
 import ClickableArea from "../../clickableArea/clickableArea.jsx";
 
 function PostCard({ post, onDelete }) {
-  const user = useContext(userContext);
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   return (
     <div className={classes.postCard}>
-    <ClickableArea onClick={() => {navigate("/posts/" + post.id)}}>
+      <ClickableArea
+        onClick={() => {
+          navigate("/posts/" + post.id);
+        }}
+      >
         <em>By: {post.author.username}</em>
         <em>{post.publishDate}</em>
         <p>{post.title}</p>
-      <div className={classes.bottomSide}>
-        <div className={classes.left}>
-          <IconWithCount
-            iconSrc={"/comment.svg"}
-            count={post.comments.length}
-          />
-          <IconWithCount iconSrc={"/heart.svg"} count={post.likes} />
-        </div>
-        <div className={classes.right}>
-          {blogApi.isAdmin(user) && (
-            <>
+        <div className={classes.bottomSide}>
+          <div className={classes.left}>
+            <IconWithCount
+              iconSrc={"/comment.svg"}
+              count={post.comments.length}
+            />
+            <IconWithCount iconSrc={"/heart.svg"} count={post.likes} />
+          </div>
+          <div className={classes.right}>
+            {post.editableByUser && (
               <IconButton
                 iconSrc="/edit.svg"
                 onClick={() => {
                   navigate("/posts/edit/" + post.id);
                 }}
               />
+            )}
+            {post.deletableByUser && (
               <IconButton
                 iconSrc="/delete.svg"
                 onClick={() => {
-                  onDelete(post.id)
+                  onDelete(post.id);
                 }}
               />
-            </>
-          )}
+            )}
+          </div>
         </div>
-      </div>
-    </ClickableArea>
+      </ClickableArea>
     </div>
   );
 }
