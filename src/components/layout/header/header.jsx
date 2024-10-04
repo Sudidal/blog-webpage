0978703982a9bae0d7629b13bb3dec0b6a0318bf
oxide <mohammedhaider15mo@gmail.com>
2024-10-04 21:41:53 +0300

    Style: cool neumorphism buttons + better heading icons

--- src/components/layout/header/header.jsx
+++ src/components/layout/header/header.jsx
@@ -1,36 +1,38 @@
 import PropTypes from "prop-types";
 import { useContext } from "react";
 import { userContext } from "../../../contexts/userContext.jsx";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import classes from "./header.module.css";
 import ThemeSwitch from "../../themeSwitch/themeSwitch.jsx";
+import IconButton from "../../iconButton/iconButton.jsx";
 
-function Header({logout}) {
+function Header({ logout }) {
   const user = useContext(userContext);
+  const navigate = useNavigate()
 
   return (
     <header className={classes.header}>
       <nav>
         <div className={classes.left}>
           <h1>
-            <Link to="/" className="unstyled-link">Oxide Blog</Link>
+            <Link to="/" className="unstyled-link">
+              Oxide Blog
+            </Link>
           </h1>
           {user && <em>Welcome, {user.username}</em>}
         </div>
         <div className={classes.right}>
+          <IconButton iconSrc={"/write.svg"} onClick={() => {navigate("/posts/new")}} />
           <ThemeSwitch />
-          <button onClick={logout}>Log Out</button>
-          <Link to={"/posts/new"} className="button-link">
-            +
-          </Link>
+          <IconButton iconSrc={"/logout.svg"} onClick={logout} />
         </div>
       </nav>
     </header>
   );
 }
 
-Header.propTypes ={
-  logout: PropTypes.func
-}
+Header.propTypes = {
+  logout: PropTypes.func,
+};
 
 export default Header;
