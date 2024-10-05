b0ff91b88f8a178e2c984b9a574c7fca9163e65a
oxide <mohammedhaider15mo@gmail.com>
2024-10-05 10:19:52 +0300

    Refactor: remove unnecessary ClickableArea component

--- src/components/posts/postCard/postCard.jsx
+++ src/components/posts/postCard/postCard.jsx
@@ -3,50 +3,48 @@ import PropTypes from "prop-types";
 import { useNavigate } from "react-router-dom";
 import IconWithCount from "../../iconWithCount/iconWithCount.jsx";
 import IconButton from "../../iconButton/iconButton.jsx";
-import ClickableArea from "../../clickableArea/clickableArea.jsx";
 import PrettyDate from "../../prettyDate/prettyDate.jsx";
 
 function PostCard({ post, onDelete }) {
   const navigate = useNavigate();
 
   return (
-    <div className={classes.postCard}>
-      <ClickableArea
-        onClick={() => {
-          navigate("/posts/" + post.id);
-        }}
-      >
-        <em>By: {post.author.username}</em>
-        <PrettyDate isoString={post.publishDate}></PrettyDate>
-        <p>{post.title}</p>
-        <div className={classes.bottomSide}>
-          <div className={classes.left}>
-            <IconWithCount
-              iconSrc={"/comment.svg"}
-              count={post.comments.length}
-            />
-            <IconWithCount iconSrc={"/heart.svg"} count={post.likes} />
-          </div>
-          <div className={classes.right}>
-            {post.editableByUser && (
-              <IconButton
-                iconSrc="/edit.svg"
-                onClick={() => {
-                  navigate("/posts/edit/" + post.id);
-                }}
-              />
-            )}
-            {post.deletableByUser && (
-              <IconButton
-                iconSrc="/delete.svg"
-                onClick={() => {
-                  onDelete(post.id);
-                }}
-              />
-            )}
-          </div>
+    <div
+      className={`${classes.postCard} clickable`}
+      onClick={() => {
+        navigate("/posts/" + post.id);
+      }}
+    >
+      <em>By: {post.author.username}</em>
+      <PrettyDate isoString={post.publishDate}></PrettyDate>
+      <p>{post.title}</p>
+      <div className={classes.bottomSide}>
+        <div className={classes.left}>
+          <IconWithCount
+            iconSrc={"/comment.svg"}
+            count={post.comments.length}
+          />
+          <IconWithCount iconSrc={"/heart.svg"} count={post.likes} />
         </div>
-      </ClickableArea>
+        <div className={classes.right}>
+          {post.editableByUser && (
+            <IconButton
+              iconSrc="/edit.svg"
+              onClick={() => {
+                navigate("/posts/edit/" + post.id);
+              }}
+            />
+          )}
+          {post.deletableByUser && (
+            <IconButton
+              iconSrc="/delete.svg"
+              onClick={() => {
+                onDelete(post.id);
+              }}
+            />
+          )}
+        </div>
+      </div>
     </div>
   );
 }
